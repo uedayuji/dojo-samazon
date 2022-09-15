b5ffa0431a3f875c5587a67c5b682c518f1924b5
uedayuji <yueda@sejuku.net>
2022-09-15 12:13:23 +0900

    update products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
@@ -9,14 +10,17 @@ class ProductsController < ApplicationController
   end
 
   def create
+    # アクション後
   end
 
   def edit
   end
 
   def update
+    # アクション後
   end
 
   def destroy
+    # アクション後
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
