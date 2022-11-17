6daa08359e501079ed94d555c7dd8d6d9a518e6a
uedayuji <yueda@sejuku.net>
2022-11-17 12:59:21 +0900

    5.3まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   resources :products
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
   resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
